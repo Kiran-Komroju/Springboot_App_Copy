3f5d717569a4d18a2646ee8f869159beac896b59
kiran-komroju <komrojukiran@gmail.com>
2025-12-18 14:36:13 +0530

    demo file

---
+++ src/main/java/com/bettersw/assignment/demo/BetterswDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bettersw.assignment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterswDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterswDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bettersw/assignment/demo/BetterswDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bettersw.assignment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterswDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
