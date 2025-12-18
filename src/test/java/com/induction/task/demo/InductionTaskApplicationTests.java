09b70b1ed285cbf7bb8991f30206fc15f0275c87
kiran-komroju <komrojukiran@gmail.com>
2025-12-18 14:24:53 +0530

    3.5.8

---
+++ src/test/java/com/induction/task/demo/InductionTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.induction.task.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InductionTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
