8c0bee0d1345d463c81e664298090cfebdb55c25
kiran-komroju <komrojukiran@gmail.com>
2025-12-18 13:19:06 +0530

    java 21 version

---
+++ src/main/java/com/induction/task/demo/InductionTaskApplication.java
@@ -0,0 +1,13 @@
+package com.induction.task.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InductionTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InductionTaskApplication.class, args);
+	}
+
+}
